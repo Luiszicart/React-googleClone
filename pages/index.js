6fc844fa670d5b8bb8acfac7a5752af2e2e2cfb2
Luis A. Zapata Icart <expansion88@pm.me>
2021-04-15 16:20:24 -0300

    Front-Page

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,26 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>      
+      </Head>
+
+      {/* Headfer */}
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+        {/* Left */}
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+        </div>
+      </header>
+      {/* Body */}
+      {/* Footer */}
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
