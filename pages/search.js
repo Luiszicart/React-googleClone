6a48576ce097123d03d0ad9463dc9c1b2f5e6838
Luis A. Zapata Icart <expansion88@pm.me>
2021-04-23 23:09:02 -0300

    HeaderSearch

---
+++ pages/search.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+import Header from '../components/Header'
+
+function Search() {
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+    </div>
+  )
+}
+
+export default Search
